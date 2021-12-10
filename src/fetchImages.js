31b4f225f8ed89c1633a272d202419d2c2959fc0
Cocokringle <cocokringle.1@gmail.com>
2021-12-11 01:46:31 +0200

    done

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -1,23 +1,54 @@
+import Notiflix from 'notiflix';
+import LoadMoreBtn from  './loadMoreBtn'
 const KEY = '24739758-4c739ca612149bb371b205192';
 const BASE_URL = 'https://pixabay.com/api';
+const loadMoreBtn = new LoadMoreBtn({
+    selector: '[data-action="load-more"]',
+    hidden: true,
+  });
 
 export default class FetchImages {
     constructor(){
         this.query = '';
+        this.page = 1; 
+        this.totalHits = '';
     }
 
-    // async getImages() {
-    //     const getImg = await fetch(`${BASE_URL}/?key=${KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true`);
-    //     const imgForGallery = await getImg.json();
-    //     return imgForGallery;
-    // }
     getImages(){
-        return fetch(`${BASE_URL}/?key=${KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true`)
+        
+        return fetch(`${BASE_URL}/?key=${KEY}&q=${this.query}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
         .then(response => response.json())
         .then((data) => {
-        
-        return data;
+        if(data.hits.length === 0){
+            loadMoreBtn.hide();
+            Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+        } else{
+            this.incrementPage()
+            
+            if (this.page * 40 >= data.totalHits) {
+                this.isEndOfImg()
+            }
+            return data;
+        }
       });
+    }
+
+
+    incrementPage(){
+        this.page +=1
+    }
+
+    resetPage() {
+        this.page =1;
+    }
+
+    isEndOfImg(){
+        loadMoreBtn.hide();
+        Notiflix.Notify.success("We're sorry, but you've reached the end of search results.");
+    }
+
+    showTotalHits(hits){
+        Notiflix.Notify.success(`Hooray! We found ${hits} images.`);
     }
 
     get searchName() {
@@ -26,5 +57,13 @@ export default class FetchImages {
     
     set searchName(newQuery) {
         this.query = newQuery;
+    }
+
+    get hits() {
+        return this.totalHits;
+    }
+    
+    set hits(newTotalHits) {
+        this.totalHits = newTotalHits;
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,61 +1,106 @@
 import './css/styles.css';
 import FetchImages from './fetchImages';
+import LoadMoreBtn from  './loadMoreBtn'
+import SimpleLightbox from 'simplelightbox';
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 const refs = {
     input: document.querySelector('[name="searchQuery"]'),
     searchForm: document.querySelector('.search-form'),
     imgBox: document.querySelector('.gallery'),
+    loadMoreBtn: document.querySelector('.load-more')
 }
 const fetchImages = new FetchImages();
-// let query = '';
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '[data-action="load-more"]',
+  hidden: true,
+});
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
+
+
 
 refs.searchForm.addEventListener('submit', onSearch)
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore)
 
-function onSearch(e){
-   
-    e.preventDefault()
+
+async function onSearch(e){
+   try{  
     fetchImages.searchName = e.currentTarget.elements.searchQuery.value.trim();
-    refs.imgBox.innerHTML = '';
+    await e.preventDefault()
+    await loadMoreBtn.show();
+    await fetchImages.resetPage()
+    await clearSearchForm()
    
     if (fetchImages.searchName !== ""){
-        console.log(fetchImages.searchName)
-        fetchImages.getImages(fetchImages.searchName).then(data => {
+        
+        await fetchImages.getImages(fetchImages.searchName).then(data => {
             addGallery(data)
+            fetchImages.showTotalHits(data.totalHits)
+            
+            
         })     
     }
+  }
+  catch (error){
+    console.log(error)
+  }
+
 }
 
 async function addGallery(data){
-    try{
-        console.log('data inside:', data )
-        const markup = await data.map((item) => {
-            return `<div class="photo-card">
-            <img src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
-            <div class="info">
-              <p class="info-item">
-                <b>${item.likes}</b>
-              </p>
-              <p class="info-item">
-                <b>${item.views}</b>
-              </p>
-              <p class="info-item">
-                <b>${item.comments}</b>
-              </p>
-              <p class="info-item">
-                <b>${item.downloads}</b>
-              </p>
-            </div>
-          </div>`
+  try{
+    const markup = await data.hits.map((item) => {
+      return `<a href="${item.largeImageURL}" class="photo-card">
+      <img class="gallery__image" src="${item.webformatURL}" alt="${item.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          
+          <b>Likes</br><span class='text'>${item.likes}</span></b>
+        </p>
+        <p class="info-item">
+          
+          <b>Views</br><span class='text'>${item.views}</span></b>
+        </p>
+        <p class="info-item">
+          
+          <b>Comments</br><span class='text'>${item.comments}</span></b>
+        </p>
+        <p class="info-item">
+          
+          <b>Downloads</br><span class='text'>${item.downloads}</span></b>
+        </p>
+      </div>
+      </a>`
         });
-        console.log(markup)
         const gallery = await refs.imgBox.insertAdjacentHTML("beforeend", markup);
+        lightbox.refresh();
         return gallery;
-    }
-    catch (error){
-        console.log(error)
-    }
+  }
+  catch (error){
+    console.log(error)
+  }
      
 }
 
+async function onLoadMore(){
+  try{
+    await loadMoreBtn.enable();
+    await fetchImages.getImages()
+    await fetchImages.getImages(fetchImages.searchName,fetchImages.totalHits).then(data => {
+      addGallery(data)
+      
+    })
+    await loadMoreBtn.show();
+  }
+  catch (error){
+    console.log(error)
+  }
+  
+}
 
-
-
+function clearSearchForm(){
+  refs.imgBox.innerHTML = '';
+}
